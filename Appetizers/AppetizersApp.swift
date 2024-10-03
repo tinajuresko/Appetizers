2b1807a7933741186b56de5416ebc0010e580f00
tinajuresko <tina.juresko16@gmail.com>
2024-10-03 15:37:55 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Tina Jureško on 03.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizersTests/AppetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersTests.swift
+//  AppetizersTests
+//
+//  Created by Tina Jureško on 03.10.2024..
+//
+
+import Testing
+@testable import Appetizers
+
+struct AppetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
